212367435cccfc8f1b39c194d7634bd4b032215d
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2021-10-20 11:53:48 +0200

    feat: created crossword

--- 5kyu/word-search-puzzle-challenge.js
+++ 5kyu/word-search-puzzle-challenge.js
@@ -1,18 +1,32 @@
 const wordSearch = (words, puzzle) => {
 	const asciiUppercaseStart = 65;
 
-	const myPuzzle = {
-		size: Math.sqrt(puzzle.length),
-		columns: [],
-		navigation: {
-			directions: ['horizontal', 'vertical', 'diagonal'],
-			increments: [1, 8, 9],
-			offset: 1,
+	const crossword = {
+		build() {
+			this.size = Math.sqrt(puzzle.length);
+			this.columns = [];
+			this.results = [];
+			this.directions = ['horizontal', 'diagonal1', 'vertical', 'diagonal2'];
+			this.createColumns();
+			this.createNavigation();
 		},
 		createColumns() {
 			for (let i = 0; i < this.size; ++i) {
 				this.columns.push(String.fromCharCode(asciiUppercaseStart + i));
 			}
+		},
+		createNavigation() {
+			this.navigation = this.directions.reduce(
+				(obj, dir, i) => {
+					if (i === 0) {
+						obj[dir] = i + 1;
+					} else {
+						obj[dir] = this.size - 2 + i;
+					}
+					return obj;
+				},
+				{ offset: 1 }
+			);
 		},
 		setStartLetter(letter) {
 			this.startLetter = letter;
@@ -23,132 +37,85 @@ const wordSearch = (words, puzzle) => {
 		setRemainingLetters(letters) {
 			this.remainingLetters = letters;
 		},
-		setAdjecentIndices() {
-			this.adjecentIndices = this.getAdjecentIndices();
+		setPossibleIndexes() {
+			this.possibleIndexes = this.getPossibleIndexes();
 		},
-		setCorrectIndices() {
-			this.correctIndices = this.getCorrectIndices();
+		setSearch(status, items) {
+			this.search = { status, items };
 		},
-
 		getGridLocation(index) {
 			index += this.navigation.offset;
 			const row = Math.ceil(index / this.size);
 			const column = this.columns[index - (row - 1) * this.size - 1];
-			return [column, row];
+			return `${column}${row}`;
 		},
-		getAdjecentIndices() {
-			const { directions, increments } = this.navigation;
-			const indices = {};
-			directions.forEach((dir, i) => {
-				indices[dir] = this.remainingLetters.split('').map((_, j) => {
-					return (j + 1) * increments[i] + this.startLetterIndex;
+		getPossibleIndexes() {
+			// todo: method needs to validate grid locations of possible indexes
+			return this.directions.reduce((obj, dir) => {
+				obj[dir] = this.remainingLetters.split('').map((_, i) => {
+					return (i + 1) * this.navigation[dir] + this.startLetterIndex;
 				});
-			});
-			return indices;
+				return obj;
+			}, {});
 		},
-		getCorrectIndices() {
-			const { directions } = this.navigation;
-			let match;
-			for (let i = 0; i < directions.length; ++i) {
-				this.adjecentIndices[directions[i]].every((value, i) => {
-					match = puzzle[value] === this.remainingLetters[i];
-					console.log(match);
-				});
-				if (match) {
-					this.adjecentIndices[directions[i]].unshift(this.startLetterIndex);
-					return this.adjecentIndices[directions[i]];
-				} else if (i === 2 && !match) {
-					return ['Not Found'];
+		searchMatchingIndexes() {
+			const { possibleIndexes: indexes, directions, remainingLetters } = this;
+			let list, isMatch;
+
+			for (let i = 0; i < this.directions.length; ++i) {
+				list = indexes[directions[i]];
+				isMatch = list.every((item, i) => puzzle[item] === remainingLetters[i]);
+				if (isMatch) {
+					this.setSearch('Match Found', list);
+					break;
 				}
 			}
 		},
-		getSolution() {
-			const { directions: dirs } = this.navigation;
-			const indices = this.adjecentIndices;
-			let solution, match, correctIndices;
-
-			// find correct indices that correspond to remaining letters
-			for (let i = 0; i < dirs.length; ++i) {
-				indices[dirs[i]].every((item, i) => {
-					match = puzzle[item] === this.remainingLetters[i];
-					console.log(match);
-				});
-				if (match) {
-					correctIndices = indices[dirs[i]];
-				}
-			}
-
-			if (!correctIndices) {
-				solution = ['Word Not Found'];
-			} else {
-				// add start letter index to indices
-				correctIndices.unshift(this.startLetterIndex);
-				// transform all indices to grid locations
-				solution = correctIndices.map(index => this.getGridLocation(index));
-			}
-			console.log(solution);
-		},
-		getLocations() {
-			console.log(this.correctIndices, 'correctIndices');
-			// const locations = this.correctIndices.map(correctIndex =>
-			// 	this.getGridLocation(correctIndex)
-			// );
-			// console.log(locations);
+		addLocationsResult() {
+			let list;
+			list = [...this.search.items];
+			list.unshift(this.startLetterIndex);
+			list = list.map(index => this.getGridLocation(index));
+			this.results.push(list);
 		},
 	};
 
-	myPuzzle.createColumns();
+	crossword.build();
 
 	for (let i = 0; i < words.length; ++i) {
-		myPuzzle.setStartLetter(words[i][0]);
+		crossword.setSearch('Word not found.');
+		crossword.setStartLetter(words[i][0]);
 
 		for (let j = 0; j < puzzle.length; ++j) {
-			if (myPuzzle.startLetter === puzzle[j]) {
-				myPuzzle.setStartLetterIndex(j);
-				myPuzzle.setRemainingLetters(words[i].slice(1));
-				myPuzzle.setAdjecentIndices();
-				myPuzzle.getSolution();
-
-				break;
-				if (remainingLetters.length > 0) {
-					console.log(remainingLetters);
+			if (crossword.startLetter === puzzle[j]) {
+				crossword.setStartLetterIndex(j);
+				crossword.setRemainingLetters(words[i].slice(1));
+				crossword.setPossibleIndexes();
+				crossword.searchMatchingIndexes();
+				if (crossword.search.status === 'Match Found') {
+					crossword.addLocationsResult();
 					break;
 				}
 			}
 		}
-		break;
-		//
-		if (remainingLetters.length) {
-			results.push(
-				remainingLetters.flatMap(letterIndex => getGridLocation(letterIndex))
-			);
-		} else {
-			results.push('Word Not Found');
+		if (crossword.search.status === 'Word not found.') {
+			crossword.results.push([crossword.search.status]);
 		}
 	}
-	return 'vader';
+	return crossword.results;
 };
+
+// console.log(
+// 	wordSearch(
+// 		['HELLO', 'WORLD'],
+// 		'FHKEFFHDFEOGIOPVFLDKOIAQFLWIHQRMUOTOXNRIAAESRUOFCUHHELTUFJJSNJDO'
+// 	)
+// );
 
 console.log(
 	wordSearch(
-		['HELLO', 'WORLD'],
-		'FHKEFFHDFEOGIOPVFLDKOIAQFLWIHQRMUOTOXNRIAAESRUOFCUHHELTUFJJSNJDO'
+		['PARROT', 'FISH', 'FOX', 'CAT', 'DOG'],
+
+		'FHKEFFDRGFEOGIOPVSFLDKOIAQIFLWIHQRMOUOTOXNRIPAAXSRUOFUFISHELTUFFJJSNJDOGREFGHHHHH'
 	)
 );
-
-// getRemainingIndices(letters) {
-// 	for (let i = 0; i < directions.length; ++i) {
-// 		directionIndices = getRemainingIndices(
-// 			letters,
-// 			directions[i],
-// 			startLetterIndex
-// 		);
-// 		areMatchWithPuzzleLetters = directionIndices.every(
-// 			(dirIndex, i) => puzzle[dirIndex] === letters[i]
-// 		);
-// 		if (areMatchWithPuzzleLetters) {
-// 			directionIndices.unshift(startLetterIndex);
-// 			return directionIndices;
-// 		}
-// 	}
-// },
